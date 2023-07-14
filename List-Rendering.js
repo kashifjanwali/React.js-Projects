e30f55976220a37ab1bb04745f7ba92c9801bef2
Kashif Hussain <kh9732494@gmail.com>
2023-07-14 10:59:44 +0500

    Showing List of Images
    Rendering lists in React

---
+++ List-Rendering.js
@@ -0,0 +1,17 @@
+export default function List_Work() {
+  let show_images = [
+    "https://media.istockphoto.com/id/1146517111/photo/taj-mahal-mausoleum-in-agra.jpg?s=612x612&w=0&k=20&c=vcIjhwUrNyjoKbGbAQ5sOcEzDUgOfCsm9ySmJ8gNeRk=",
+    "https://media.istockphoto.com/id/1146517111/photo/taj-mahal-mausoleum-in-agra.jpg?s=612x612&w=0&k=20&c=vcIjhwUrNyjoKbGbAQ5sOcEzDUgOfCsm9ySmJ8gNeRk=",
+    "https://media.istockphoto.com/id/1146517111/photo/taj-mahal-mausoleum-in-agra.jpg?s=612x612&w=0&k=20&c=vcIjhwUrNyjoKbGbAQ5sOcEzDUgOfCsm9ySmJ8gNeRk=",
+    "https://media.istockphoto.com/id/1146517111/photo/taj-mahal-mausoleum-in-agra.jpg?s=612x612&w=0&k=20&c=vcIjhwUrNyjoKbGbAQ5sOcEzDUgOfCsm9ySmJ8gNeRk=",
+  ];
+  let show_items = show_images.map((image) => <li><img className="images" src={image} /></li>);
+
+  return (
+    <>
+      <h1>Image list</h1>
+      {/* <button onClick={list_items()}>Click to show numbers</button> */}
+      <ol>{show_items}</ol>
+    </>
+  );
+}
